b62751b9d9da4199f0f081e19b085c4f9621d6c5
Raphael Leiteritz <48442682+leiterrl@users.noreply.github.com>
2021-06-21 14:58:03 +0200

    don't check style on package build

--- tools/manylinux_setup_script.sh
+++ tools/manylinux_setup_script.sh
@@ -10,4 +10,4 @@ export PATH=/opt/python/"${PY_VER}"/bin:$PATH
 /opt/python/"${PY_VER}"/bin/pip install --upgrade --no-cache-dir pip
 /opt/python/"${PY_VER}"/bin/pip install --no-cache-dir scons numpy
 
-/opt/python/"${PY_VER}"/bin/scons -j2 SG_ALL=0 SG_BASE=1 SG_PYTHON=1
+/opt/python/"${PY_VER}"/bin/scons -j2 SG_ALL=0 SG_BASE=1 SG_PYTHON=1 CHECK_STYLE=0
