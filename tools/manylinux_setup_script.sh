dea8f2822cb2f4f466b0a8d6ee8c9af9aee04497
Raphael Leiteritz <48442682+leiterrl@users.noreply.github.com>
2021-06-21 14:24:15 +0200

    add pip upgrade step + add python to PATH

--- tools/manylinux_setup_script.sh
+++ tools/manylinux_setup_script.sh
@@ -5,6 +5,9 @@ set -e
 
 PY_VER="cp37-cp37m"
 
+export PATH=/opt/python/"${PY_VER}"/bin:$PATH
+
+/opt/python/"${PY_VER}"/bin/pip install --upgrade --no-cache-dir pip
 /opt/python/"${PY_VER}"/bin/pip install --no-cache-dir scons
 
 /opt/python/"${PY_VER}"/bin/scons -j2 SG_ALL=0 SG_BASE=1 SG_PYTHON=1
