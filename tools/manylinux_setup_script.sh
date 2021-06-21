c3502ddc31e5a7a1fd751ce4d814a892d1610308
Raphael Leiteritz <48442682+leiterrl@users.noreply.github.com>
2021-06-21 16:15:54 +0200

    Update manylinux_setup_script.sh

--- tools/manylinux_setup_script.sh
+++ tools/manylinux_setup_script.sh
@@ -6,8 +6,20 @@ set -e
 PY_VER="cp37-cp37m"
 
 export PATH=/opt/python/"${PY_VER}"/bin:$PATH
+python --version
 
-/opt/python/"${PY_VER}"/bin/pip install --upgrade --no-cache-dir pip
-/opt/python/"${PY_VER}"/bin/pip install --no-cache-dir scons numpy
+pip install --upgrade --no-cache-dir pip
+pip install --no-cache-dir scons numpy
+
+cd io
 
 /opt/python/"${PY_VER}"/bin/scons -j2 SG_ALL=0 SG_BASE=1 SG_PYTHON=1 CHECK_STYLE=0
+
+export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH
+
+scons -j8 SG_JAVA=0 RUN_BOOST_TESTS=0 CHECK_STYLE=0 PYDOC=1 LINKFLAGS=-s
+
+python setup.py bdist_wheel
+
+auditwheel repair dist/pysgpp-0.0.0-py3-none-any.whl --plat manylinux2014_x86_64
+
