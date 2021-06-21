30f1a04f60c1aec868106da602935ab1acd44b1b
Raphael Leiteritz <48442682+leiterrl@users.noreply.github.com>
2021-06-21 14:21:29 +0200

    specify python executables

--- tools/manylinux_setup_script.sh
+++ tools/manylinux_setup_script.sh
@@ -3,6 +3,8 @@
 # exit when any command fails
 set -e
 
-pip install scons
+PY_VER="cp37-cp37m"
 
-scons -j2 SG_ALL=0 SG_BASE=1 SG_PYTHON=1
+/opt/python/"${PY_VER}"/bin/pip install --no-cache-dir scons
+
+/opt/python/"${PY_VER}"/bin/scons -j2 SG_ALL=0 SG_BASE=1 SG_PYTHON=1
